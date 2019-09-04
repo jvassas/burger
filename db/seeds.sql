57b94dde5bebe19711177b32674abc5d7f7b1ff8
jvassas <jakevassas@gmail.com>
2019-09-04 00:03:22 -0400

    sql files and folder set up

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Galley Boy Burger', FALSE);
